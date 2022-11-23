7d3da43c5786f504aeb67ce5b6cbc3d85ccc828e
Yashas <210070095@iitb.ac.in>
2022-11-23 23:35:20 +0530

    modified encoder to take vector input

--- priorityencoder.vhdl
+++ priorityencoder.vhdl
@@ -2,55 +2,55 @@ library ieee;
 use iee.std_logic_1164.all;
 
 entity pen is
-	port(o2, o1, o0: out std_logic; i7, i6, i5, i4, i3, i2, i1, i0: in std_logic);
+	port(o2, o1, o0: out std_logic; i: in std_logic_vector(7 downto 0));
 end entity pen;
 
 architecture pen_bhv of pen is
 begin
  	--I have assumed the lower priority bits to be 'don't care' conditions
- 	if(i7 = '0' and i6 = '0' and i5 = '0' and i4 = '0' and i3 = '0' and i2 = '0' and i1 = '0' and i0 = '1') then
+ 	if(i(7) = '0' and i(6) = '0' and i(5) = '0' and i(4) = '0' and i(3) = '0' and i(2) = '0' and i(1) = '0' and i(0) = '1') then
 		o2 <= '0';
 		o1 <= '0';
 		o0 <= '0';
 	end if;
 	
-	if(i7 = '0' and i6 = '0' and i5 = '0' and i4 = '0' and i3 = '0' and i2 = '0' and i1 = '1') then
+	if(i(7) = '0' and i(6) = '0' and i(5) = '0' and i(4) = '0' and i(3) = '0' and i(2) = '0' and i(1) = '1') then
 		o2 <= '0';
 		o1 <= '0';
 		o0 <= '1';
 	end if;
 	
-	if(i7 = '0' and i6 = '0' and i5 = '0' and i4 = '0' and i3 = '0' and i2 = '1') then
+	if(i(7) = '0' and i(6) = '0' and i(5) = '0' and i(4) = '0' and i(3) = '0' and i(2) = '1') then
 		o2 <= '0';
 		o1 <= '1';
 		o0 <= '0';
 	end if;
 	
-	if(i7 = '0' and i6 = '0' and i5 = '0' and i4 = '0' and i3 = '1') then
+	if(i(7) = '0' and i(6) = '0' and i(5) = '0' and i(4) = '0' and i(3) = '1') then
 		o2 <= '0';
 		o1 <= '1';
 		o0 <= '1';
 	end if;
 	
-	if(i7 = '0' and i6 = '0' and i5 = '0' and i4 = '1') then
+	if(i(7) = '0' and i(6) = '0' and i(5) = '0' and i(4) = '1') then
 		o2 <= '1';
 		o1 <= '0';
 		o0 <= '0';
 	end if;
 	
-	if(i7 = '0' and i6 = '0' and i5 = '1') then
+	if(i(7) = '0' and i(6) = '0' and i(5) = '1') then
 		o2 <= '1';
 		o1 <= '0';
 		o0 <= '1';
 	end if;
 	
-	if(i7 = '0' and i6 = '1') then
+	if(i(7) = '0' and i(6) = '1') then
 		o2 <= '1';
 		o1 <= '1';
 		o0 <= '0';
 	end if;
 	
-	if(i7 = '1') then
+	if(i(7) = '1') then
 		o2 <= '1';
 		o1 <= '1';
 		o0 <= '1';
