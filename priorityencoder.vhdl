1ea0f9970e5e6c30f5559dd764dedb7412b305ee
Yashas M Salian <102826237+yashas-msalian@users.noreply.github.com>
2022-11-26 22:01:43 +0530

    made additional change

--- priorityencoder.vhdl
+++ priorityencoder.vhdl
@@ -11,4 +11,4 @@ begin
 	pen_o(2) <= pen_i(0) or pen_i(1) or pen_i(2) or pen_i(3);
 	pen_o(1) <= pen_i(0) or pen_i(1) or pen_i(4) or pen_i(5);
 	pen_o(0) <= pen_i(0) or pen_i(2) or pen_i(4) or pen_i(6);
-end pen_bhv;
+end PEN_bhv;
